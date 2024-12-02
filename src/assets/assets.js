e18ccba6ea8761073325c19e8f9931b7170adb94
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-12-01 19:38:50 -0600

    🍱 Update assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,33 +1,31 @@
-import history_icon from './history_icon.png'
 import menu_icon from './menu_icon.png'
 import plus_icon from './plus_icon.png'
 import question_icon from './question_icon.png'
-import setting_icon from './setting_icon.png'
-import bulb_icon from './bulb_icon.png'
+import rag_icon from './rag_icon.png'
 import compass_icon from './compass_icon.png'
-import gallery_icon from './gallery_icon.png'
-import mic_icon from './mic_icon.png'
 import user_icon from './user_icon.png'
-import youtube_icon from './youtube_icon.png'
-import message_icon from './message_icon.png'
-import code_icon from './code_icon.png'
+import mail_icon from './mail_icon.png'
+import calendar_icon from './calendar_icon.png'
 import send_icon from './send_icon.png'
-import gemini_icon from './gemini_icon.png'
+import zolkin_icon from './zolkin_icon.png'
+import conversation_icon from './conversation_icon.png'
+import upload_file_icon from './upload_file_icon.png'
+import google_icon from './google_icon.png'
+import openai_icon from './openai_icon.png'
 
 export const assets = {
-    history_icon,
     menu_icon,
     plus_icon,
     question_icon,
-    setting_icon,
-    bulb_icon,
+    rag_icon,
     compass_icon,
-    gallery_icon,
-    mic_icon,
     user_icon,
-    youtube_icon,
-    message_icon,
-    code_icon,
+    mail_icon,
+    calendar_icon,
     send_icon,
-    gemini_icon
+    zolkin_icon, 
+    conversation_icon, 
+    upload_file_icon, 
+    google_icon,
+    openai_icon
 }

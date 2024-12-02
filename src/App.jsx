d81595dccdca768bee4748422da0ae717f11770d
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-12-02 00:20:50 -0600

    ✨ Refactor App component: integrate React Router for navigation, add LoginCard and Chat components, and implement authentication check for chat access.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Sidebar from "./components/Sidebar/Sidebar.jsx";
-import Main from "./components/Main/Main.jsx";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginCard from "./components/login/LoginCard.jsx";
+import Sidebar from "./components/sidebar/Sidebar.jsx";
+import Chat from "./components/chat/Chat.jsx";
 
 
 const App = () => {
+    const isAuth = true;
     return (
-        <>
-            <Sidebar/>
-            <Main/>
-        </>
+        <Router>
+            <Routes>
+                <Route path='/login' element={<LoginCard />} />
+                <Route path='/chat' element={
+                    isAuth ? <><Sidebar /><Chat /></> : <Navigate to="/login" replace />
+                } />
+                <Route path="*" element={<Navigate to="/login" replace />} />
+            </Routes>
+        </Router>
     )
 }
 

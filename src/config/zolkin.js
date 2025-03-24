35c46ee7131e176a8c1c761c55ee44965be84e06
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2025-03-24 02:40:53 -0600

    💡 Add documentation for new RAG parameter

--- src/config/zolkin.js
+++ src/config/zolkin.js
@@ -1,32 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 axios.defaults.withCredentials = true;
-
 
 /**
  * Función asíncrona para realizar una solicitud GET a la API.
  * @param {string} prompt - El prompt del usuario.
  * @param {string} conversationId - El ID de la conversación.
+ * @param {string} use_rag - Si el prompt requiere usar la herramienta de RAG.
  * @returns {Promise<string>} - La respuesta desde la API.
  */
 const getChatResponse = async (prompt, thread_id, use_rag) => {
-    const backend_url = process.env.BACKEND_URL;
-    try {
-        // Realizar la solicitud GET con Axios
-        const response = await axios.get(`${backend_url}/chat/`, {
-            withCredentials: true,
-            params: {
-                prompt: prompt,
-                thread_id: thread_id,
-                use_rag: use_rag
-            }
-        });
-        // Retornar solo la respuesta contenida en el JSON
-        return response.data.response;
-    } catch (error) {
-        console.error('Error al realizar la solicitud a la API:', error);
-        throw new Error('No se pudo obtener una respuesta válida de la API.');
-    }
+  const backend_url = process.env.BACKEND_URL;
+  try {
+    // Realizar la solicitud GET con Axios
+    const response = await axios.get(`${backend_url}/chat/`, {
+      withCredentials: true,
+      params: {
+        prompt: prompt,
+        thread_id: thread_id,
+        use_rag: use_rag,
+      },
+    });
+    // Retornar solo la respuesta contenida en el JSON
+    return response.data.response;
+  } catch (error) {
+    console.error("Error al realizar la solicitud a la API:", error);
+    throw new Error("No se pudo obtener una respuesta válida de la API.");
+  }
 };
-
 
 export default getChatResponse;
